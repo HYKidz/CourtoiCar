71dcc7bcc73756fbec4d23f22301be575ec06a7c
Samahel Brassard <HYKinteractive@hotmail.com>
2023-09-07 11:57:21 -0400

    Init, but vs is shit

---
+++ Assets/Scripts/FirebaseController/FirebaseInit.cs
@@ -0,0 +1,18 @@
+using Firebase;
+using UnityEngine;
+
+
+public class FirebaseInit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
